ebbfc6de5b92da6365c7e525366e679b997fa133
harishamd <48724092+harishamd@users.noreply.github.com>
2020-05-04 00:45:25 +0700

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,7 +9,7 @@ class Admin extends CI_Controller {
 
     public function loginadmin() {
         $where = [
-            'email' => $this->input->post('email'),
+            'username' => $this->input->post('username'),
             'katasandi' => $this->input->post('katasandi'),
         ];
         $admin = $this->AdminModel->getAdmin($where);
@@ -17,7 +17,7 @@ class Admin extends CI_Controller {
         if ($admin->num_rows() > 0) {
             $this->session->set_userdata('admin', $admin->row_array());
             $this->session->set_flashdata('message', 'Login success');
-            redirect('page/home');
+            redirect('page/homeadmin');
         } else {
             $this->session->set_flashdata('message', 'Login fail');
             redirect('page/loginadmin');
